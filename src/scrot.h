4650bfe49e0d1bd869ec514accfb8aec0e33cfe2
Daniel T. Borelli <danieltborelli@gmail.com>
2019-06-09 09:15:56 -0300

    Update manpage and copyright notice.

--- src/scrot.h
+++ src/scrot.h
@@ -4,6 +4,7 @@ Copyright 1999-2000 Tom Gilbert <tom@linuxbrit.co.uk,
                                   gilbertt@linuxbrit.co.uk,
                                   scrot_sucks@linuxbrit.co.uk>
 Copyright 2009      James Cameron <quozl@us.netrek.org>
+Copyright 2019      Daniel T. Borelli <danieltborelli@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
