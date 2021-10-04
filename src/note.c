47d3b6c1652238e47fab412a2973d7617f90dc22
IFo Hancroft <contact@ifohancroft.com>
2021-10-04 22:40:10 +0300

    Added copyright

--- src/note.c
+++ src/note.c
@@ -3,6 +3,7 @@
 Copyright 2019-2021 Daniel T. Borelli <daltomi@disroot.org>
 Copyright 2021      Guilherme Janczak <guilherme.janczak@yandex.com>
 Copyright 2021      Peter Wu <peterwu@hotmail.com>
+Copyright 2021      IFo Hancroft <contact@ifohancroft.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
