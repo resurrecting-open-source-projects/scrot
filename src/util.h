a4c5735778ca887ec5d1ab57d96378673c8cecc4
Daniel T. Borelli <danieltborelli@gmail.com>
2021-12-08 20:04:23 -0300

    Copyright: remove space

--- src/util.h
+++ src/util.h
@@ -1,6 +1,6 @@
 /* util.h
 
-Copyright 2021      Guilherme Janczak <guilherme.janczak@yandex.com>
+Copyright 2021 Guilherme Janczak <guilherme.janczak@yandex.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
