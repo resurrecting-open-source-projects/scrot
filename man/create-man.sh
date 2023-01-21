3cc5eebe398ad8c651c25a0aaa8f01fb42f68d2c
Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
2023-01-20 23:55:24 -0300

    Updated manpage

--- man/create-man.sh
+++ man/create-man.sh
@@ -16,9 +16,9 @@ txt2man -d "$T2M_DATE" -t $T2M_NAME -r $T2M_NAME-$T2M_VERSION -s $T2M_LEVEL -v "
 #--------------------------------------------------------
 
 # Put here all data for your first manpage (in T2M lines)
-T2M_DATE="09 Jan 2023"
+T2M_DATE="20 Jan 2023"
 T2M_NAME=scrot
-T2M_VERSION=1.8
+T2M_VERSION=1.8.1
 T2M_LEVEL=1
 T2M_DESC="command line screen capture utility"
 create-man
