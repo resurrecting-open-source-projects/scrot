af61a264dc6086aa0c669451c3abe68ade7ce8b2
NRK <nrk@disroot.org>
2023-04-07 06:20:11 +0600

    fix displaying incorrect error message
    the scrotGrab* functions do not necessarily set errno.
    
    to reproduce run: `scrot -s` and then cancle the selection with keyboard
    press.

--- src/scrot.c
+++ src/scrot.c
@@ -133,7 +133,7 @@ int main(int argc, char *argv[])
     }
 
     if (!image)
-        err(EXIT_FAILURE, "no image grabbed");
+        errx(EXIT_FAILURE, "no image grabbed");
 
     if (opt.note)
         scrotNoteDraw(image);
