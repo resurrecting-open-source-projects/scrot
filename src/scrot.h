5aff0acadc31607ac423b1d203a9656bf0851ecd
Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
2023-04-10 23:53:51 -0300

    Added or updated rights for NRK and Guilherme Janczak

--- src/scrot.h
+++ src/scrot.h
@@ -1,7 +1,7 @@
 /* scrot.h
 
-Copyright 2021 Guilherme Janczak <guilherme.janczak@yandex.com>
-Copyright 2022 Daniel T. Borelli <danieltborelli@gmail.com>
+Copyright 2021-2023 Guilherme Janczak <guilherme.janczak@yandex.com>
+Copyright 2022      Daniel T. Borelli <danieltborelli@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
